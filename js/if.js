063ccc697983d5f15da1c232ec8168b055281643
Rimantas <belauzas@yahoo.com>
2020-11-30 10:30:24 +0200

    If grieztas palyginimas

--- js/if.js
+++ js/if.js
@@ -94,3 +94,11 @@ if (day === 'mon') {
 } else {
     console.log('Not a week day... 🤔');
 }
+
+console.clear();
+
+if (0 === false) {
+    console.log('POzityvas');
+} else {
+    console.log('NEgatyvas');
+}
